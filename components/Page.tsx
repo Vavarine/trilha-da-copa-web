1076dfb24c7bb5869061ea27b80f9950133abc37
Leonardo Scalabrin <leojscalabrin@gmail.com>
2022-11-25 00:21:42 -0300

    page slider

---
+++ components/Page.tsx
@@ -0,0 +1,10 @@
+export function Page() {
+    return (
+        <div className="sticker-wrapper">
+            <div className="sticker">Bonita figurinha aqui</div>
+            <div className="sticker">Bonita figurinha aqui</div>
+            <div className="sticker">Bonita figurinha aqui</div>
+            <div className="sticker">Bonita figurinha aqui</div>
+        </div>
+    )
+}
