accbfae521bb7395637f458ed7397163f58f6098
Evailson Barbosa <evailson@agenciaeplus.com.br>
2022-11-25 16:20:46 -0300

    ref: src path

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
-import '../styles/globals.css'
-import '../styles/PageSlider.css';
-import '../styles/Page.css'
-import "../styles/globals.css";
+import "../styles/PageSlider.css";
+import "../styles/Page.css";
 import "../styles/home.css";
 import type { AppProps } from "next/app";
 import { AuthProvider } from "../contexts/AuthContext";
+
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
