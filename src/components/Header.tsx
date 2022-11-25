4d0519cecd04c07537a806245bc02f2b406418e7
Leonardo Scalabrin <leojscalabrin@gmail.com>
2022-11-25 16:18:50 -0300

    algumas coisa ai

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ export function Header() {
     return (
         <header className="header-bar">
             <img className="logo" src="https://iili.io/H3S72g1.png" alt="Logo" />
+            <img className="login" src="https://iili.io/HFo57qv.png" alt="Login"/>
         </header>
     )
 }
--- src/components/PageSlider.tsx
+++ src/components/PageSlider.tsx
@@ -2,9 +2,21 @@ import React from 'react';
 import HTMLFlipBook from "react-pageflip";
 import { Page } from './Page';
 
+
+
 const PageSlider = () => {
+
   return (
-      <HTMLFlipBook className='sticker-wrapper page' width={900} height={850} >
+      <HTMLFlipBook className='sticker-wrapper page' width={693} height={1031} >
+        <div className='page cover'></div>
+        <div className='page cover'>
+          <img src="https://iili.io/HFzOiap.jpg" alt="Capa do álbum"/>
+        </div>
+        <div className='page back-cover'>
+          <h2 className='back-cover-title'>
+            Na trilha da copa 2022
+          </h2>
+        </div>
         <div className="page page-1">
           <Page />
         </div>
@@ -15,6 +27,21 @@ const PageSlider = () => {
           <Page />
         </div>
         <div className="page page-4">
+          <Page />
+        </div>
+        <div className="page page-5">
+          <Page />
+        </div>
+        <div className="page page-6">
+          <Page />
+        </div>
+        <div className="page page-7">
+          <Page />
+        </div>
+        <div className="page page-8">
+          <Page />
+        </div>
+        <div className="page page-9">
           <Page />
         </div>
       </HTMLFlipBook>
